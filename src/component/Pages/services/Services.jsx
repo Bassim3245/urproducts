9a6d03d1edccccd2ef6705e831472ac6da3ef90d
Bassim3245 <bassimhussein443@gmail.com>
2024-11-16 09:47:19 +0300

    edit css

--- src/component/Pages/services/Services.jsx
+++ src/component/Pages/services/Services.jsx
@@ -13,7 +13,7 @@ function Services() {
 
   const fetchServiceData = async () => {
     try {
-      const response = await axios.get(`${BackendUrl}Services/`);
+      const response = await axios.get(`${BackendUrl}/Services/`);
       setServices(response.data);
     } catch (error) {
       console.error('Error fetching service data:', error);
